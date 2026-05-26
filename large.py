b2d86284852fd503b2d98aa16144e892ffc3b198
chetan <156156147+chetan3622@users.noreply.github.com>
2026-05-26 16:23:08 +0000

    large number

---
+++ large.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+if a>b:
+    print("The larger number is:", a)
+elif b>a:
+    print("The larger number is:", b)
+else:    print("Both numbers are equal.")
