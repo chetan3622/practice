65576baffd16f64a1fdd8e029ddb467ef9b976c1
chetan <156156147+chetan3622@users.noreply.github.com>
2026-06-09 17:40:27 +0000

    print satement

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,5 @@
 n=int(input("Enter a number: "))
 if n%2==0:
-    print("The number is even.")
+    print("given number is even.")
 else:   
-    print("The number is odd.")
+    print("given number is odd.")
