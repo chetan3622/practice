90d146ebc334714607ce8687cef3d4490330d197
chetan <156156147+chetan3622@users.noreply.github.com>
2026-06-09 17:42:48 +0000

    change statement

--- large.py
+++ large.py
@@ -1,7 +1,7 @@
 a=int(input("Enter a number: "))
 b=int(input("Enter another number: "))
 if a>b:
-    print("The larger number is:", a)
+    print("The largest number is:", a)
 elif b>a:
-    print("The larger number is:", b)
+    print("The largest number is:", b)
 else:    print("Both numbers are equal.")
