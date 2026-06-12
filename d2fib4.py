fbb62bb75102c2a1d292b4bfec54f03a71d5d7fc
chetan <156156147+chetan3622@users.noreply.github.com>
2026-06-12 11:56:18 +0530

    Fix input prompt for Fibonacci term count

--- d2fib4.py
+++ d2fib4.py
@@ -4,6 +4,6 @@ def fib(n):
     if n==1:
         return 1
     return fib(n-1)+fib(n-2)
-n=int(input("Enter the number of terms: "))
+n=int(input("Enter the number: "))
 for i in range(n):
     print(fib(i))
